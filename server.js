e5cffbf15b54e87002c9dfa8158494a1beaa2896
Bluebird <stephany1445@hotmail.com>
2020-09-20 22:29:11 -0700

    added .gitignore Added Get Request in otrder to read the data file db.json

--- server.js
+++ server.js
@@ -18,9 +18,33 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+// API's
+// GET Request: reads the db.json and returns all saved notes as JSON
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname + "/db/db.json"), (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// POST Request: Receives a new note => save on the request body => adds it to the db.json => returns new note to client
+// app.post("/api/notes", (req, res) => {
+//     const notes
+//     fs.readFile(path.join(__dirname + "/db/db.json"), (err, data) => {
+//         if (err) { throw err }
+//         notes =JSON.parse(data)
+//     })
+//     const newEntryNote = {
+//         title: req.body.title
+//     }
+// })
